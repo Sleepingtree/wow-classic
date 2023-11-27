0c6ed67b4c6b38ea4851fd075f95b330aa1876ec
Aaron Grabow <ajgrabow@gmail.com>
2023-11-26 19:26:58 -0500

    added infinit scrolling

--- src/componets/profileCarousel.tsx
+++ src/componets/profileCarousel.tsx
@@ -1,26 +1,35 @@
-import { useState } from "react";
+import { useEffect } from "react";
 import { api } from "~/utils/api";
 import ProfileBaseballCard from "./profileBaseballCard";
 
 export default function ProfileCarousel() {
-  const [page, _setPage] = useState(0);
   const { data: signedUpUsers, fetchNextPage } =
     api.profile.getSignedUpUsers.useInfiniteQuery(
-      { limit: 10 },
+      { limit: 5 },
       {
         getNextPageParam: (lastPage) => lastPage.nextCursor,
       },
     );
-  // const handleFetchNextPage = () => {
-  //   fetchNextPage();
-  //   setPage((prev) => prev + 1);
-  // };
 
-  // const handleFetchPreviousPage = () => {
-  //   setPage((prev) => prev - 1);
-  // };
-  // data will be split in pages
-  const toShow = signedUpUsers?.pages[page]?.items;
+  const toShow = signedUpUsers?.pages.flatMap((page) => page.items);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const offsetHeight = document.documentElement.offsetHeight;
+      const innerHeight = window.innerHeight;
+      const scrollTop = document.documentElement.scrollTop;
+
+      const hasReachedBottom = offsetHeight - (innerHeight + scrollTop) <= 10;
+
+      if (hasReachedBottom) {
+        fetchNextPage();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
     <>
